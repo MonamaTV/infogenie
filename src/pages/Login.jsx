c47ea9c5600fa445d1747340b5ecc26f64c6662c
Tadima <19066791722k23@bootcamp.wethinkcode.co.za>
2024-07-27 11:12:29 +0200

    axios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Login = () => {
-  return <div className="container">Login</div>;
+  return (
+    <div className="container h-screen w-screen flex flex-row justify-center items-center">
+      <form className="w-1/2" action="">
+        <div>
+          <label htmlFor="">Username</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Username</label>
+          <input type="password" />
+        </div>
+      </form>
+      <img className="w-1/2" src="./login.jpg" alt="" />
+    </div>
+  );
 };
 
 export default Login;
